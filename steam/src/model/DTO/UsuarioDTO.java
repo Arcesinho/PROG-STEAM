4c4a7f235d4813aafd70064b19d30b903c32348f
adrian <adrian.balboa.1608@gmail.com>
2026-01-31 00:37:41 +0100

    Creada estructura básica de carpetas

---
+++ steam/src/model/DTO/UsuarioDTO.java
@@ -0,0 +1,14 @@
+package model.DTO;
+
+
+
+public class UsuarioDTO {
+
+    private int id;
+    private String nombreUsuario;
+    private String email;
+    private String contrasenia;
+    private String nombreReal;
+    private String pais;
+
+}
---
+++ steam/src/wiring/Wiring.java
@@ -0,0 +1,4 @@
+package wiring;
+
+public class Wiring {
+}
